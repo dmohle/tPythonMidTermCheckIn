fc83e80c918961b8f2d06449768f55c7d8ada692
dmohle <10038913+dmohle@users.noreply.github.com>
2023-10-14 19:23:50 -0700

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,61 +1,101 @@
 # Main.py
 # driver file for the animal zoo program
 # last update 10/13/23 by dH
+# last update 10/14/23
 
-from Hyena import Hyena
+
 from Animal import Animal
+from Hyena import Hyena
 
-# Create two Hyena objects
-hyena_01 = Hyena("Shenzi", 5, "winter", "brown color", "75 pounds", "from Friguia Park")
-hyena_02 = Hyena("Banzai", 3, "summer", "tan color", "80 pounds", "from Friguia Park")
+from _datetime import date
 
-# Think about this: How many arguments were used to create hyena_01? Does the Hyena class have a constructor
-# with five parameters? Will this code work if you create a hyena with five arguments?
+# Create lists of the species
+list_of_hyenas = []
+list_of_lions = []
+list_of_tigers = []
+list_of_bears = []
 
-# Output information about the Hyena object
-print(f"Hyena Name: {hyena_01.name}")
-print(f"Species: {hyena_01.species}")
-print(f"Age: {hyena_01.age}")
-print(f"Color: {hyena_01.color}")
-print(f"Weight: {hyena_01.weight}")
-print(f"Originating Zoo: {hyena_01.originating_zoo}")
-print(f"Sound made: {hyena_01.make_sound()}")
+# This is needed for the calc birthday stuff.
+current_date = date.today()
+current_year = current_date.year
+
+
+def calc_birth_date(the_season, the_years):
+    year_of_birthday = int(current_year) - int(the_years)
+
+    the_birth_day = ""
+
+    if "spring" in the_season:
+        the_birth_day = str(year_of_birthday) + "-03-21"
+    elif "summer" in the_season:
+        the_birth_day = str(year_of_birthday) + "-06-21"
+    elif "fall" in the_season:
+        the_birth_day = str(year_of_birthday) + "-09-21"
+    elif "winter" in the_season:
+        the_birth_day = str(year_of_birthday) + "-12-21"
+
+    return the_birth_day
+
+
+def process_one_line(one_line):
+    # Create variables to help parse arrivingAnimals.txt
+    a_species = ""
+    a_gender = ""
+    age_in_years = 99
+    season = ""
+    color = ""
+    weight = ""
+    origin_01 = ""
+    origin_02 = ""
+
+    print(one_line)
+    groups_of_words = one_line.strip().split(",")
+    print(groups_of_words)
+    single_words = groups_of_words[0].strip().split(" ")
+    age_in_years = single_words[0]
+    a_gender = single_words[3]
+    a_species = single_words[4]
+    single_words = groups_of_words[1].strip().split(" ")
+    season = single_words[2]
+    color = groups_of_words[2].strip();
+    weight = groups_of_words[3].strip();
+    origin_01 = groups_of_words[4].strip();
+    origin_02 = groups_of_words[5].strip();
+
+    from_zoo = origin_01 + ", " + origin_02
+
+    birth_day = calc_birth_date(season, age_in_years)
+
+    if "hyena" in a_species:
+        # Create a hyena object.
+        my_hyena = Hyena("aName", "anID", birth_day, color, a_gender, weight, from_zoo, current_date)
+        # fill in name and ID
+        my_hyena.name = Hyena.get_hyena_name(my_hyena)
+        my_hyena.animal_id = "Hy" + str(Hyena.numOfHyenas)
+        # add to the hyena list
+        list_of_hyenas.append(my_hyena)
+
+
+# Open arrivingAnimals.txt and read it one line at a time
+# Open the file in read mode
+file_path = r"C:\2023spring\pythonRoot\dataFiles\arrivingAnimals.txt"
+with open(file_path, "r") as file:
+    # Iterate through the file line by line
+    for line in file:
+        process_one_line(line)
 
 # Access the static variable numOfHyenas
 print(f'Number of Hyenas created: {Hyena.numOfHyenas}')
-
-# we know the Hyena class here (because we imported it) so we can call the class method that gets the hyena names
-# call the class method named load_hyena_names()
-print("******************")
-# hyena_01.load_hyena_names()
-print("******************")
-# Hyena.load_hyena_names()
-print("******************")
-
-# Create a list of animals
-animals = []
-
-# Create an Animal with seven arguments
-animal = Animal("lion", "Scar", 3, "spring", "tan", "425 pounds", "Zanzibar")
-# add the new animal to the end of the list
-animals.append(animal)
-
-# Create another hyena
-another_hyena = Hyena("Zig", 5, "summer", "black spots", "80 pounds", "Zanzibar")
-# add the new hyena to the animals list
-animals.append(another_hyena)
-
-
-# Create an animal with two arguments
-my_new_animal = Animal.create_with_species_and_name_only("bear", "Smokey")
-animals.append(my_new_animal)
-
-# Print information about the created objects in the animals list
-for i, animal in enumerate(animals):
-    print(f"Animal {i}: {animal.species}, {animal.name}, {animal.age} years old")
 
 # Access the static variable numOfAnimals
 print(f"\n\nNumber of animals created: {Animal.numOfAnimals}")
 
 # Output the static variable numOfHyenas
 print(f"\n\nNumber of hyenas created: {Hyena.numOfHyenas}")
+
+# output the animals
+
+for hyena in list_of_hyenas:
+    print(hyena.animal_id + ", " + hyena.name + "; birthdate: " + str(hyena.birth_date) + "; " + hyena.color +
+          "; " + hyena.gender + "; " + hyena.weight + "; " + hyena.originating_zoo + "; arrived: " +
+          str(hyena.date_arrival))
