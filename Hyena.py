4b48e4e43a56074fafb091853ae4a8f613b7de2d
dmohle <10038913+dmohle@users.noreply.github.com>
2023-10-14 19:23:09 -0700

    Add files via upload

--- Hyena.py
+++ Hyena.py
@@ -5,44 +5,39 @@ class Hyena(Animal):
     # create a static class variable to keep track of the number of hyenas created
     numOfHyenas = 0
 
-    # create a class method that creates a list of hyena names from the animalNames.txt text file
-    # a class method will not belond to an object of the class (notice the @classmethod decoration) and
-    # look at the parameter of this class method, it is cls not self
-    @classmethod
-    def load_hyena_names(cls):
-        # Define the file path
-        file_path = r'C:\2023fall\python\dataFiles\animalNames.txt'
+    # Create the hyena sound
+    hyena_sound = " laugh...laugh "
 
-        # Initialize an empty list to store hyena names
-        hyena_names = []
+    # Create a list of hyena names.
+    list_of_hyena_names = []
 
-        # Open the file and read its content
-        # using with will close the file after this code block is done executing
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-
-        print("\n type of lines is " + str(type(lines)))
+    file_path = r'C:\2023fall\python\dataFiles\animalNames.txt'
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
 
         # Iterate through the lines in the file
+        line_num = 1
         for line in lines:
-            if line.strip() == "Hyena Names:":
-                is_hyena_section = True
-            elif is_hyena_section and line.strip():
-                # Add the hyena name to the list
-                hyena_names.extend(line.strip().split(', '))
+            if line_num == 3:
+                list_of_hyena_names.extend(line.strip().split(', '))
+                break
+            else:
+                line_num += 1
 
-        # Print the list of hyena names
-        print("Hyena Names:")
-        for name in hyena_names:
-            print(name)
-
-    # __init__ is the constructor method. Note how "self" is used here. "self" refers to the object created
-    def __init__(self, name, age, birth_season, color, weight, originating_zoo):
+    def __init__(self, name="a_name", animal_id="an_id", birth_date="2099-01-01", color="a_color", gender="a_gender",
+                 weight="a_weight", originating_zoo="a_zoo", date_arrival="2099-01-01"):
         # Increment the static variable numOfHyenas when a new Hyena object is created
         Hyena.numOfHyenas += 1
 
         # Call the constructor of the parent class (Animal) with 'Hyena' as the species
-        super().__init__("hyena", name, age, birth_season, color, weight, originating_zoo)
+        super().__init__("hyena", name, animal_id, birth_date, color, gender, weight, originating_zoo, date_arrival)
 
     def make_sound(self):
-        return " laugh laugh "
+        return self.hyena_sound
+
+    # the hyena object will call this method to get an unused hyena name. pop() will remove the first element from
+    #   the list_of_hyena_names[]
+    def get_hyena_name(self):
+        return self.list_of_hyena_names.pop(0)
+
+
